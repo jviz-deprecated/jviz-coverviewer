f49a2d2bfc96e30d7d557f65bbbb21c275fdf3e5
jmjuanes <josemijuanes@gmail.com>
2016-10-13 11:10:52 +0200

    js/coverviewer.js: added data and draw configuration

--- js/coverviewer.js
+++ js/coverviewer.js
@@ -43,6 +43,47 @@ jviz.modules.coverviewer = function(opt)
   //Build the canvas layers
   this._canvas = new jviz.components.canvas({ id: this._id + '-canvas', parent: this._panel.body().id, layers: this._layers, width: this._width, height: this._height, margin: this._margins });
 
+  //Data
+  this._data = {};
+  this._data.chromosome = ''; //Data chromosome
+  this._data.start = 0; //Data start position
+  this._data.end = 0; //Data end position
+  this._data.values = {}; //Data values
+  this._data.normalized = {}; //Normalized data values
+  this._data.min = 0; //Cover data min value
+  this._data.max = 0; //Cover data max value
+
+  //Samples object
+  this._samples = {};
+  this._samples.count = 0; //Count the number of samples
+  this._samples.names = []; //Samples names
+  this._samples.active = []; //Samples active
+  this._samples.empty = []; //Samples empty array
+
+  //Draw object
+  this._draw = {};
+  this._draw.start = 0; //Draw start position
+  this._draw.end = 0; //Draw end positon
+  this._draw.length = 0; //Draw length
+  this._draw.move = false; //Draw on move
+
+  //Draw the background
+  this._draw.bg = {};
+  this._draw.bg.layer = 1; //Background layer
+  this._draw.bg.color = jviz.colors.navy; //Background lines color
+  this._draw.bg.opacity = 0; //Background lines opacity
+  this._draw.bg.stroke = 2; //Background lines stroke
+
+  //Draw the marks
+  this._draw.marks = {};
+  this._draw.marks.layer = 0; //Marks layer
+
+  //Parse the samples
+  if(typeof opt.samples !== 'undefined'){ this.samples(opt.samples); }
+
+  //Parse the data
+  (typeof opt.ajax === 'undefined') ? this.data(opt.data) : this.ajax(opt.ajax);
+
   //Return this
   return this;
 };
