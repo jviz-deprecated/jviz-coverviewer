3fbc4cb2cd7ce1b8f8f500baf4f73786f3787388
jmjuanes <josemijuanes@gmail.com>
2016-10-13 11:12:29 +0200

    js/samples.js: manage samples

---
+++ js/samples.js
@@ -0,0 +1,24 @@
+//Manage the samples
+jviz.modules.coverviewer.prototype.samples = function(data)
+{
+  //Check the data object
+  if(typeof data === 'undefined'){ return this._samples.names; }
+
+  //Check for array
+  if(jviz.is.array(data) === false){ data = [ data ]; }
+
+  //Save the names
+  this._samples.names = data;
+
+  //Count the number of samples
+  this._samples.count = data.length;
+
+  //Initialize the active samples array
+  this._samples.active = jviz.array.create(data.length, false);
+
+  //Initialize the samples empty array
+  this._samples.empty = jviz.array.zeros(data.length);
+
+  //Calulates the background lines opacity
+  this._draw.bg.opacity = (this._samples.count === 0) ? 0 : 1 / this._samples.count;
+};
