2ec74cd1a67af92f403437366ec33dd6cc7bb72e
jmjuanes <josemijuanes@gmail.com>
2016-10-14 10:29:34 +0200

    js/menu.js: build menu

---
+++ js/menu.js
@@ -0,0 +1,25 @@
+//Build the menu
+jviz.modules.coverviewer.prototype.menu = function()
+{
+  //Get the menu container id
+  var id = this._panel.head().content.id;
+
+  //Reset the menu content
+  jviz.dom.empty(id);
+
+  //Add the samples button
+  jviz.dom.append(id, { id: this._menu.samples.id, class: this._menu.samples.class, _html: this._menu.samples.text });
+
+  //Save this
+  var self = this;
+
+  //Add the preview button icon event
+  jviz.mouse.down(this._menu.samples.id, function(e){ return self.menuClickSamples(); });
+};
+
+//Click on the samples button
+jviz.modules.coverviewer.prototype.menuClickSamples = function()
+{
+  //Emit the event
+  this.emit('btn:samples');
+};
