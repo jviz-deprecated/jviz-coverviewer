e614c7ab07e5876a64ddf8fa15722907297e020d
jmjuanes <josemijuanes@gmail.com>
2016-10-18 15:36:53 +0200

    js/table.js: added rows

--- js/table.js
+++ js/table.js
@@ -28,19 +28,6 @@ jviz.modules.coverviewer.prototype.hideTable = function()
 //Build the table
 jviz.modules.coverviewer.prototype.tableBuild = function()
 {
-  //Check if table exists
-  if(this._table.build === false)
-  {
-    //Create the table container
-    jviz.dom.append(this._panel.body().id, { id: this._table.id, class: this._table.class });
-
-    //Hide the table
-    jviz.dom.style(this._table.id, 'display', 'none');
-
-    //Set table build
-    this._table.build = true;
-  }
-
   //Reset the table content
   jviz.dom.html(this._table.id, '');
 
@@ -50,16 +37,48 @@ jviz.modules.coverviewer.prototype.tableBuild = function()
   //Check the samples count
   if(this._samples.count === 0){ return; }
 
+  //Count the number of lines
+  var lines = this.tableLines();
+
+  //Add the number of lines
+  this._table.scroll.number(lines);
+
   //Read all the samples
   for(var i = 0; i < this._samples.count; i++)
   {
+    //Get the row ID
+    var row_id = this._table.row.id + Math.floor(i / this._table.row.cells).toString();
+
+    //Check the index
+    if(i % this._table.row.cells === 0)
+    {
+      //Get the row class
+      var row_class = this._table.row.class;
+
+      //Append the row
+      jviz.dom.append(this._table.id, { id: row_id, class: row_class });
+    }
+
     //Add the sample
-    this.tableSampleAdd(i);
+    this.tableSampleAdd(i, row_id);
   }
 };
 
+//Count the number of lines
+jviz.modules.coverviewer.prototype.tableLines = function()
+{
+  //Get the number of samples
+  var count = this._samples.count;
+
+  //Get the number of cells for each row
+  var cells = this._table.row.cells
+
+  //Return the number of lines
+  return (count % cells === 0) ? count / cells : Math.floor(count / cells) + 1;
+};
+
 //Add a new sample to the table
-jviz.modules.coverviewer.prototype.tableSampleAdd = function(index)
+jviz.modules.coverviewer.prototype.tableSampleAdd = function(index, parent)
 {
   //Save this
   var self = this;
@@ -83,7 +102,7 @@ jviz.modules.coverviewer.prototype.tableSampleAdd = function(index)
   var cell_class = this._table.cell.class;
 
   //Add the cell
-  jviz.dom.append(this._table.id, { id: cell_id, class: cell_class });
+  jviz.dom.append(parent, { id: cell_id, class: cell_class });
 
   //Get the switch ID
   var switch_id = this._table.cell.switch.id + index;
