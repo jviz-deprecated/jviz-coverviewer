c7d45e5390a31a80c4091fa9d9520279e91ba818
jmjuanes <josemijuanes@gmail.com>
2016-10-18 15:51:58 +0200

    js/build.js: build module

---
+++ js/build.js
@@ -0,0 +1,13 @@
+//Coverviewer build
+jviz.modules.coverviewer.prototype.build = function()
+{
+
+  //Create the table container
+  jviz.dom.append(this._panel.body().id, { id: this._table.id, class: this._table.class });
+
+  //Hide the table
+  jviz.dom.style(this._table.id, 'display', 'none');
+
+  //Add the scroll event
+  this._table.scroll = new jviz.plugins.scrollBlock({ id: this._table.id, num: 1, size: this._table.cell.size });
+};
