132665fd7850d4fcb608c6e83da1819208fadb8a
jmjuanes <josemijuanes@gmail.com>
2016-10-18 14:06:04 +0200

    js/table.js: major update

--- js/table.js
+++ js/table.js
@@ -29,48 +29,96 @@ jviz.modules.coverviewer.prototype.hideTable = function()
 jviz.modules.coverviewer.prototype.tableBuild = function()
 {
   //Check if table exists
-  if(this._table.build === true){ return; }
+  if(this._table.build === false)
+  {
+    //Create the table container
+    jviz.dom.append(this._panel.body().id, { id: this._table.id, class: this._table.class });
 
-  //Create the table container
-  jviz.dom.append(this._panel.body().id, { id: this._table.id, class: this._table.class });
+    //Hide the table
+    jviz.dom.style(this._table.id, 'display', 'none');
 
-  //Hide the table
-  jviz.dom.style(this._table.id, 'display', 'none');
+    //Set table build
+    this._table.build = true;
+  }
 
-  //Set table build
-  this._table.build = true;
-};
-
-//Build the table samples
-jviz.modules.coverviewer.prototype.tableBuildSamples = function()
-{
   //Reset the table content
   jviz.dom.html(this._table.id, '');
+
+  //Reset the switch elements list
+  this._table.cell.switch.el = [];
+
+  //Check the samples count
+  if(this._samples.count === 0){ return; }
 
   //Read all the samples
   for(var i = 0; i < this._samples.count; i++)
   {
-    //Get the sample name
-    var sample_name = this._samples.names[i];
-
-    //Check if sample is active
-    var sample_active = this._samples.active.list[i];
-
-    //Get the sample index
-    var sample_index = this._samples.active.index.indexOf(i);
-
-    //Get the sample color
-    var sample_color = (sample_index === -1) ? this._samples.default : this._colors[sample_index];
-
-    //Get the cell id
-    var cell_id = this._table.cell.id + i;
-
-    //Get the cell switch ID
-    var cell_
-
-    //Add the cell
-    jviz.dom.append(this._table.id, { id: cell_id, class: this._table.cell.class });
-
-    //
+    //Add the sample
+    this.tableSampleAdd(i);
   }
+};
+
+//Add a new sample to the table
+jviz.modules.coverviewer.prototype.tableSampleAdd = function(index)
+{
+  //Save this
+  var self = this;
+
+  //Get the sample name
+  var sample_name = this._samples.names[index];
+
+  //Check if sample is active
+  var sample_active = this._samples.active.list[index];
+
+  //Get the sample index
+  var sample_index = this._samples.active.index.indexOf(index);
+
+  //Get the sample color
+  var sample_color = (sample_index === -1) ? this._samples.default : this._colors[sample_index];
+
+  //Get the cell id
+  var cell_id = this._table.cell.id + index;
+
+  //Get the cell class
+  var cell_class = this._table.cell.class;
+
+  //Add the cell
+  jviz.dom.append(this._table.id, { id: cell_id, class: cell_class });
+
+  //Get the switch ID
+  var switch_id = this._table.cell.switch.id + index;
+
+  //Build the switch element
+  var switch_el = new jviz.components.switch({ id: switch_id, parent: cell_id });
+
+  //Add the switch event
+  switch_el.on('change', function(value){ self.tableSampleToggle(index, value); });
+
+  //Save the switch element
+  //this._table.cell.switch.el.push(switch_el);
+
+  //Get the text id
+  var text_id = this._table.cell.text.id + index;
+
+  //Get the text class
+  var text_class = this._table.cell.text.class;
+
+  //Add the sample name
+  jviz.dom.append(cell_id, { _tag: 'span', id: text_id, class: text_class, _html: sample_name });
+
+  //Add the color
+  jviz.dom.style(text_id, 'color', this._samples.color[index]);
+};
+
+//Show the sample
+jviz.modules.coverviewer.prototype.tableSampleToggle = function(index, value)
+{
+  //Check the value
+  (value === true) ? this.showSample(index) : this.hideSample(index);
+
+  //Get the sample text id
+  var id = this._table.cell.text.id + index;
+
+  //Update the sample color
+  jviz.dom.style(id, 'color', this._samples.color[index]);
 };
