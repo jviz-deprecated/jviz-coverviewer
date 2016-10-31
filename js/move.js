0b33f0a7230f02b0377ba484f84b96e8aa206d01
jmjuanes <josemijuanes@gmail.com>
2016-10-31 11:22:30 +0100

    js/move.js: move the graphic

---
+++ js/move.js
@@ -0,0 +1,53 @@
+//Coverviewer move start
+jviz.modules.coverviewer.prototype.moveStart = function(pos)
+{
+  //Check the position
+  if(typeof pos === 'undefined'){ return this; }
+
+  //Save the start position
+  this._draw.start = parseInt(pos);
+
+  //Clear the background
+  this.backgroundClear();
+
+  //Clear the label
+  this.labelClear();
+
+  //Draw this new position
+  this.draw({ background: false });
+
+  //Return this
+  return this;
+};
+
+//Move to a position
+jviz.modules.coverviewer.prototype.moveTo = function(pos)
+{
+  //Check the position
+  if(typeof pos === 'undefined'){ return this; }
+
+  //Set the draw start position
+  this._draw.start = parseInt(pos);
+
+  //Draw this new position
+  this.draw({ background: false });
+
+  //Return this
+  return this;
+};
+
+//Stop moving
+jviz.modules.coverviewer.prototype.moveStop = function(pos)
+{
+  //Check the position
+  if(typeof pos === 'undefined'){ return this; }
+
+  //Set the draw start position
+  this._draw.start = parseInt(pos);
+
+  //Draw with the background
+  this.draw({ background: true });
+
+  //Return this
+  return this;
+};
