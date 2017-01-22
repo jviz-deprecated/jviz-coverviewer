0d9ff0f8b1631dc1c26ecdb75bd4e2b55b49b29e
jmjuanes <josemijuanes@gmail.com>
2017-01-22 17:42:16 +0100

    js/zoom.js: manage zoom

---
+++ js/zoom.js
@@ -0,0 +1,60 @@
+//Apply a zoom level
+jviz.modules.coverviewer.prototype.zoom = function(value)
+{
+  //Check the zoom value
+  if(typeof value !== 'number'){ return this._zoom.value; }
+
+  //Save the zoom value
+  this._zoom.value = (this._zoom.fixed === true) ? this._zoom.default : value;
+
+  //Check the zoom min value
+  if(this._zoom.value < this._zoom.min){ this._zoom.value = this._zoom.min; }
+
+  //Check the zoom max value
+  if(this._zoom.value > this._zoom.max){ this._zoom.value = this._zoom.max; }
+
+  //Return this
+  return this;
+};
+
+//Updte the zoom value
+jviz.modules.coverviewer.prototype.zoomUpdate = function()
+{
+  //Calculate the min zoom value
+  this._zoom.min = this._draw.width / this._cover.length;
+
+  //Apply the zoom level
+  this.zoom(this._zoom.value);
+
+  //Update the draw length
+  this._draw.length = this._draw.width / this._zoom.value;
+
+  //Return this
+  return this;
+};
+
+//Zoom in
+jviz.modules.coverviewer.prototype.zoomIn = function()
+{
+  //Check if zoom is fixed
+  if(this._zoom.fixed === true){ return this; }
+
+  //Update the zoom
+  this.zoom(this._zoom.value + this._zoom.increment);
+
+  //Draw all
+  return this.draw();
+};
+
+//Zoom out
+jviz.modules.coverviewer.prototype.zoomOut = function()
+{
+  //Check if zoom is fixed
+  if(this._zoom.fixed === true){ return this; }
+
+  //Update the zoom
+  this.zoom(this._zoom.value - this._zoom.increment);
+
+  //Draw all
+  return this.draw();
+};
