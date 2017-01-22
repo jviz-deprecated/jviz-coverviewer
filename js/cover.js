d6aa64037b07c89e531666f8510c337ef201226a
jmjuanes <josemijuanes@gmail.com>
2017-01-22 13:37:37 +0100

    js/cover.js: fixed coverage object

--- js/cover.js
+++ js/cover.js
@@ -1,14 +1,14 @@
 //Import data
-jviz.modules.coverviewer.prototype.data = function(data)
+jviz.modules.coverviewer.prototype.cover = function(data)
 {
   //Check the data
   if(typeof data !== 'object')
   {
     //Check if has data
-    if(this._data.has === false){ return false; }
+    if(this._cover.has === false){ return false; }
 
     //Return the actual chromosome, start and end position and the values
-    return { chromosome: this._data.chromosome, start: this._data.start, end: this._data.end, values: this._data.values };
+    return { chromosome: this._cover.chromosome, start: this._cover.start, end: this._cover.end, values: this._cover.values };
   }
 
   //Check the chromosome
@@ -27,34 +27,34 @@ jviz.modules.coverviewer.prototype.data = function(data)
   this.loading(true);
 
   //Save the chromosome
-  this._data.chromosome = data.chromosome;
+  this._cover.chromosome = data.chromosome;
 
   //Save the start position
-  this._data.start = parseInt(data.start);
+  this._cover.start = parseInt(data.start);
 
   //Save the end position
-  this._data.end = parseInt(data.end);
+  this._cover.end = parseInt(data.end);
 
   //Save the values
-  this._data.values = data.values;
+  this._cover.values = data.values;
 
   //Set has data as true
-  this._data.has = true;
+  this._cover.has = true;
 
   //Parse the data values
   this.dataParse();
 
   //Initialize the region draw start position
-  this._draw.region.start = this._data.start;
+  this._draw.region.start = this._cover.start;
 
   //Get the start point
-  var point_start = jviz.math.format(this._data.start, '.');
+  var point_start = jviz.math.format(this._cover.start, '.');
 
   //Get the end point
-  var point_end = jviz.math.format(this._data.end, '.');
+  var point_end = jviz.math.format(this._cover.end, '.');
 
   //Update the panel detail
-  this._panel.el.detail('<b>Chromosome ' + this._data.chromosome + '</b>&nbsp;' + point_start + ' - ' + point_end);
+  this._panel.el.detail('<b>Chromosome ' + this._cover.chromosome + '</b>&nbsp;' + point_start + ' - ' + point_end);
 
   //Draw the data
   this.draw({ background: true });
@@ -70,59 +70,59 @@ jviz.modules.coverviewer.prototype.data = function(data)
 jviz.modules.coverviewer.prototype.dataParse = function()
 {
   //Reset the min value
-  this._data.min = 99999999;
+  this._cover.min = 99999999;
 
   //Reset the max value
-  this._data.max = 0;
+  this._cover.max = 0;
 
   //Read the values
-  for(var key in this._data.values)
+  for(var key in this._cover.values)
   {
     //Get the cover values
-    var cover = this._data.values[key];
+    var cover = this._cover.values[key];
 
     //Get the min value
-    //Math.min(jviz.array.min(cover), this._data.min);
+    //Math.min(jviz.array.min(cover), this._cover.min);
 
     //Get the max value
-    //Math.max(jviz.array.max(cover), this._data.max);
+    //Math.max(jviz.array.max(cover), this._cover.max);
 
     //Get the cover min value
     var cover_min = jviz.array.min(cover);
 
     //Compare
-    if(cover_min < this._data.min){ this._data.min = cover_min; }
+    if(cover_min < this._cover.min){ this._cover.min = cover_min; }
 
     //Get the cover max value
     var cover_max = jviz.array.max(cover);
 
     //Compare the values
-    if(this._data.max < cover_max){ this._data.max = cover_max; }
+    if(this._cover.max < cover_max){ this._cover.max = cover_max; }
   }
 
   //Check 0 value in max
-  this._data.max = Math.max(1, this._data.max)
+  this._cover.max = Math.max(1, this._cover.max)
 
   //Check 0 value in min
-  this._data.min = Math.max(1, this._data.min);
+  this._cover.min = Math.max(1, this._cover.min);
 
   //Reset the normalized data values
-  this._data.normalized = {};
+  this._cover.normalized = {};
 
   //Transform the original data to a new data
-  for(var key in this._data.values)
+  for(var key in this._cover.values)
   {
     //Initialize the normalized position
-    this._data.normalized[key] = [];
+    this._cover.normalized[key] = [];
 
     //Get all the coverages
-    for(var j = 0; j < this._data.values[key].length; j++)
+    for(var j = 0; j < this._cover.values[key].length; j++)
     {
       //Calculate the new height
-      var h = this._data.values[key][j]/this._data.max;
+      var h = this._cover.values[key][j]/this._cover.max;
 
       //Save the value
-      this._data.normalized[key][j] = h * this._draw.height;
+      this._cover.normalized[key][j] = h * this._draw.height;
     }
   }
 
