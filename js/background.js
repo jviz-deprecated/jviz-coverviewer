d0b6d9c0499304063075cfd5fd6697cad4eb434b
jmjuanes <josemijuanes@gmail.com>
2017-01-22 17:42:54 +0100

    js/background.js: fixed cover

--- js/background.js
+++ js/background.js
@@ -20,10 +20,13 @@ jviz.modules.coverviewer.prototype.backgroundDraw = function()
   var p = draw.margin.left;
 
   //Read all the positions
-  for(var i = this._draw.region.start; i < this._draw.region.end; i++)
+  for(var i = 0; i < this._draw.width; i++)
   {
+    //Get the position
+    var pos = Math.floor(this._draw.start + i / this._zoom.value);
+
     //Get the cover array
-    var cover = (typeof this._data.normalized[i] === 'undefined') ? this._samples.empty : this._data.normalized[i];
+    var cover = (typeof this._cover.values[pos] === 'undefined') ? this._samples.empty : this._cover.values[pos];
 
     //Draw the lines
     for(var j = 0; j < this._samples.count; j++)
