4a1544f07bddffac648a47a08b9003dece45722f
jmjuanes <josemijuanes@gmail.com>
2017-01-22 21:44:29 +0100

    js/menu.js: display menu buttons. Added menu click events

--- js/menu.js
+++ js/menu.js
@@ -1,31 +1,54 @@
 //Build the menu
 jviz.modules.coverviewer.prototype.menu = function()
 {
-  //Get the menu container id
-  //var id = this._panel.head().content.id;
-
-  //Reset the menu content
-  //jviz.dom.empty(id);
-
-  //Add the samples button
-  //jviz.dom.append(id, { id: this._menu.samples.id, class: this._menu.samples.class, _html: this._menu.samples.text });
+  //Save this
+  var self = this;
 
   //Add the samples button
   this._panel.el.addBtn(this._menu.samples);
 
-  //Save this
-  var self = this;
+  //Add the zoom in button
+  this._panel.el.addIcon(this._menu.zoomin);
+
+  //Add the zoom out button
+  this._panel.el.addIcon(this._menu.zoomout);
 
   //Add the preview button icon event
-  jviz.dom.event(this._menu.samples.id, 'click', function(e){ return self.menuClickSamples(); });
+  jviz.dom.event(this._menu.samples.id, 'click', function(e){ return self.menuSamples(); });
+
+  //Add the zoom in button event
+  jviz.dom.event(this._menu.zoomin.id, 'click', function(el){ return self.menuZoomIn(); });
+
+  //Add the zoom out button event
+  jviz.dom.event(this._menu.zoomout.id, 'click', function(el){ return self.menuZoomOut(); });
 };
 
 //Click on the samples button
-jviz.modules.coverviewer.prototype.menuClickSamples = function()
+jviz.modules.coverviewer.prototype.menuSamples = function()
 {
   //Emit the event
   this._events.emit('btn:samples');
 
   //Show/Hide the samples table
   this.table();
+};
+
+//Click on the zoom in button
+jviz.modules.coverviewer.prototype.menuZoomIn = function()
+{
+  //Emit the event
+  this._events.emit('btn:zoom-in');
+
+  //Call the zoom in method
+  this.zoomIn();
+};
+
+//Click on the zoom out button
+jviz.modules.coverviewer.prototype.menuZoomOut = function()
+{
+  //Emit the event
+  this._events.emit('btn:zoom-out');
+
+  //Call the zoom out method
+  this.zoomOut();
 };
