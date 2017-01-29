a7e3f4c037ecb26a2a5143bf0982fc448fe2b7a7
jmjuanes <josemijuanes@gmail.com>
2017-01-29 21:40:49 +0100

    js/coverviewer.js: minor changes

--- js/coverviewer.js
+++ js/coverviewer.js
@@ -253,7 +253,7 @@ jviz.modules.coverviewer = function(opt)
   this._selection.click.end = 0; //Click end point
   this._selection.click.orientation = 0; //Click orientation
   this._selection.click.move = false; //Selection move is active
-  this._Selection.click.type = ''; //Selection type
+  this._Selection.click.type = 0; //Selection type
 
   //Selection button
   this._selection.btn = {};
