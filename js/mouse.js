94a59625dafd9ce257f2007574de0ff75f575310
jmjuanes <josemijuanes@gmail.com>
2017-02-21 12:13:51 +0100

    js/mouse.js: big update

--- js/mouse.js
+++ js/mouse.js
@@ -1,33 +1,18 @@
-//Register the mouse events
-jviz.modules.coverviewer.prototype.mouseEvent = function()
-{
-  //Save this
-  var self = this;
-
-  //Get the last layer ID
-  var id = this._canvas.el.layerID(this._canvas.layers - 1);
-
-  //Save this
-  var self = this;
-
-  //Add the mouse down event
-  jviz.commons.mouse.down(id, function(e, x, y){ return self.mouseDown(e, x, y); });
-
-  //Add the mouse move event
-  jviz.commons.mouse.move(id, function(e, x, y){ return self.mouseMove(e, x, y); });
-
-  //ADd the mouse up event
-  jviz.commons.mouse.up(id, function(e, x, y){ return self.mouseUp(e, x, y); });
-};
-
 //Mouse click down event
 jviz.modules.coverviewer.prototype.mouseDown = function(e, x, y)
 {
   //Check for loading
   if(this.loading() === true){ return;}
 
+  //Check for selection active
+  if(this._selection.active === true)
+  {
+    //Call the selection down
+    return this.selectionDown(x, y);
+  }
+
   //Activate the move
-  this._draw.move = true;
+  //this._draw.move = true;
 
   //Activate the click first
   this._draw.click.first = true;
@@ -36,19 +21,7 @@ jviz.modules.coverviewer.prototype.mouseDown = function(e, x, y)
   this._draw.click.point = x;
 
   //Save the start position
-  this._draw.click.start = this._draw.region.start;
-
-  //Add the cursor
-  jviz.cursor.set('move');
-
-  //Clear the background
-  this.backgroundClear();
-
-  //Clear the label
-  this.labelClear();
-
-  //Emit the drag start event
-  this._events.emit('drag:start', this._draw.region.start, this._draw.region.end, this._draw.region.length);
+  this._draw.click.start = this._draw.start;
 };
 
 //Mouse move event
@@ -57,6 +30,35 @@ jviz.modules.coverviewer.prototype.mouseMove = function(e, x, y)
   //Check for loading
   if(this.loading() === true){ return; }
 
+  //Check the selection move is active
+  if(this._selection.active === true)
+  {
+    //Call the selection move
+    return this.selectionMove(x, y);
+  }
+
+  //Check the click first
+  if(this._draw.click.first === true)
+  {
+    //Set move as true
+    this._draw.move = true;
+
+    //Remove the click first
+    this._draw.click.first = false;
+
+    //Emit the drag start event
+    this._events.emit('drag:start', this._draw.start, this._draw.end, this._draw.length);
+
+    //Clear the background
+    this.backgroundClear();
+
+    //Clear the label
+    //this.labelClear();
+
+    //Add the cursor
+    jviz.cursor.set('move');
+  }
+
   //Check for move
   if(this._draw.move === true)
   {
@@ -64,47 +66,61 @@ jviz.modules.coverviewer.prototype.mouseMove = function(e, x, y)
     var diff = this._draw.click.point - x;
 
     //Calculate the start point
-    this._draw.region.start = this._draw.click.start + diff;
+    this._draw.start = this._draw.click.start + diff / this._zoom.value;
 
     //Draw the region
-    this.draw({ background: false });
+    this.draw();
 
     //Emit the drag move event
-    this._events.emit('drag:move', this._draw.region.start, this._draw.region.end, this._draw.region.length);
+    this._events.emit('drag:move', this._draw.start, this._draw.end, this._draw.length);
 
     //Continue
     return;
   }
 
-  //Check click first
-  //if(this.cover.clickfirst === true) { this.cover.clickfirst = false; }
-
   //Get the draw zone
-  var draw = this._canvas.draw();
+  var draw = this._canvas.el.draw();
 
   //Show the hover position
   if(draw.margin.left <= x && x <= draw.margin.left + draw.width)
   {
     //Calculate the position
-    this._draw.position = Math.floor(this._draw.region.start + x - draw.margin.left);
+    this._draw.position = Math.floor(this._draw.start + x - draw.margin.left);
 
     //Draw the label
-    this.labelDraw(x, y);
+    //this.labelDraw(x, y);
   }
+
+  //Exit
+  return;
 };
 
 //Mouse click up event
 jviz.modules.coverviewer.prototype.mouseUp = function(e, x, y)
 {
+  //Check the selection up
+  if(this._selection.active === true)
+  {
+    //Call the selection up
+    return this.selectionUp(x, y);
+  }
+
+  //Check if draw move is active
+  if(this._draw.move === true)
+  {
+    //Emit the drag stop event
+    this._events.emit('drag:stop', this._draw.start, this._draw.end, this._draw.length);
+
+    //Remove the cursor
+    jviz.cursor.remove('move');
+  }
+
   //Set draw move as false
   this._draw.move = false;
 
-  //Remove the cursor
-  jviz.cursor.remove('move');
+  //Reset the click first
+  this._draw.click.first = false;
 
   //Draw the region
-  this.draw({ background: true });
-
-  //Emit the drag stop event
-  this._events.emit('drag:stop', this._draw.region.start, this._draw.region.end, this._draw.region.length);
+  this.draw();
 };
